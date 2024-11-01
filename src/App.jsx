79fa9b1ed916a908ef0c709a37a61a425947a8cd
vkrakeshjangid@gmail.com <121666589+rakeshjan9090@users.noreply.github.com>
2024-11-01 21:35:53 +0530

    Styling in React - module css, tailwindcss add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+      <div className="w-full h-screen bg-zinc-900 p-4">
+        <div className="w-44 h-32 rounded-md bg-red-600 p-5 text-white">
+          <h3>hello kase ho</h3>
+        </div>
+      </div>
+
+  );
+}
+
+export default App;
